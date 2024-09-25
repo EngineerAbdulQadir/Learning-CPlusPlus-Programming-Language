c1e6835f70220504430234358a82e51144c0f10f
EnggAbdulQadir <abdulqadirimran001@gmail.com>
2024-09-25 23:57:11 +0500

    Update

---
+++ Class 03/code.cpp
@@ -0,0 +1,275 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /// Conditional Statement
+    // If Else
+
+    // Condition (Fill It Chat Gpt)
+    // int n = 45;
+
+    // if (n >= 0) {
+    //     cout << "N Is Postive\n";
+    // } else {
+    //     cout << "N Is Negative\n";
+    // }
+
+    // Condition (Fill It Chat Gpt)
+    // int n = -45;
+
+    // if (n >= 0) {
+    //     cout << "N Is Postive\n";
+    // } else {
+    //     cout << "N Is Negative\n";
+    // }
+
+    // Condition (Fill It Chat Gpt)
+    // int age;
+    // cout << "Enter Your Age :";
+    // cin >> age;
+
+    // if (age >= 18) {
+    //     cout << "You Can Vote";
+    // } else {
+    //     cout << "You Can't Vote";
+    // }
+
+    // Condition (Fill It Chat Gpt)
+    // int n;
+    // cout << "Enter The Number :";
+    // cin >> n;
+
+    // if (n%2 == 0) {
+    //     cout << "Even";
+    // } else {
+    //     cout << "Odd";
+    // }
+
+    // Else If (Introduced)
+    // Condition (Fill It Chat Gpt)
+    // int marks;
+    // cout << "Enter Your Marks :";
+    // cin >> marks;
+
+    // if (marks >= 90) {
+    //     cout << "A\n";
+    // } else if (marks >= 80 && marks < 90 ) {
+    //     cout << "B\n";
+    // } else {
+    //     cout << "C\n";
+    // }
+
+    // Practice Question (Find Character Uppercase or Lowercase)
+    // Way 01
+    // char ch;
+    // cout << "Enter The Character :";
+    // cin >> ch;
+
+    // if (ch >= 'a' && ch <= 'z') {
+    //     cout << "lowercase\n";
+    // } else {
+    //     cout << "UPPERCASE\n";
+    // }
+
+    // Way 02
+    // char ch;
+    // cout << "Enter The Character :";
+    // cin >> ch;
+
+    // if (ch >= 65 && ch <= 90) {
+    //     cout << "UPPERCASE\n";
+    // } else {
+    //     cout << "lowercase\n";
+    // }
+
+    /// Ternary Statement
+    // int n = 45;
+    // cout << (n >= 0 ? "Positive" : "Negative") <<endl;
+
+    // int m = -45;
+    // cout << (m >= 0 ? "Positive" : "Negative") <<endl;
+
+    /// Loops
+    // Print Numbers 1 to 5
+    // While Loop
+    // int count = 1;
+    // while(count <= 5) {
+    //     cout << count << " ";
+    //     count++;
+    // }
+    // cout << endl;
+
+    // int n = 20;
+    // int count = 1;
+    // while(count <= n) {
+    //     cout << count << " ";
+    //     count++;
+    // }
+    // cout << endl;
+
+    // Infinite Loop
+    // int n = 50;
+    // int count = 1;
+    // while(count <= n) {
+    //     cout << count << " ";
+    // }
+    // cout << endl;
+
+    // For Loop
+    // int n = 10;
+    // for (int i=1; i<=n; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    // int n = 10;
+    // for(int i=1; i<=n; i = i+2) {
+    //     cout << i << " ";
+    // }
+
+    // cout << endl;
+
+    // Practice Question (Sum of numbers 1 to n)
+    // int n = 3;
+    // int sum = 0;
+
+    // for (int i=0; i<=n; i++) {
+    //     // sum = sum +i;
+    //     sum += i;
+    // }
+    // cout << "Sum = " << sum << endl;
+
+    // Break (Introduced)
+    // int n = 50;
+    // int sum = 0;
+
+    // for (int i=0; i<=n; i++) {
+    //     sum += i;
+    //     if (i == 5) {
+    //         break;
+    //     }
+    // }
+    // cout << "Sum = " << sum << endl;
+
+    // Practice Question (Sum of all Odd Numbers from 1 to N)
+    // int n = 50;
+    // int oddSum = 0;
+
+    // for(int i=1; i<=n; i++){
+    //     if (i%2 != 0) {
+    //         oddSum += i;
+    //     }
+    // }
+
+    // cout << "Odd Sum = " << oddSum << endl;
+
+    // Do While Loop
+    // while(3 > 5) {
+    //     cout << "Hello World!\n";
+    // }
+
+    // do {
+    //     cout << "Hello World!\n";
+    // } while(3 > 5);
+
+    // int n = 10;
+    // int i = 1;
+
+    // do {
+    //     cout << i << " ";
+    //     i++;
+    // } while (i <= n);
+    // cout << endl;
+
+    // Practice Question (Check if a number is prime or not)
+    // int n = 5;
+    // bool isPrime = true;
+
+    // for (int i=2; i<=n-1; i++) {
+    //     if(n % i == 0) {
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+
+    // if(isPrime == true) {
+    //     cout << "Prime Number\n";
+    // } else {
+    //     cout << "Non Prime Number\n";
+    // }
+
+    // int n = 9;
+    // bool isPrime = true;
+
+    // for (int i=2; i<=n-1; i++) {
+    //     if(n % i == 0) {
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+
+    // if(isPrime == true) {
+    //     cout << "Prime Number\n";
+    // } else {
+    //     cout << "Non Prime Number\n";
+    // }
+
+    // int n = 11;
+    // bool isPrime = true;
+
+    // for (int i=2; i*i <= n; i++) {
+    //     if(n % i == 0) {
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+
+    // if(isPrime == true) {
+    //     cout << "Prime Number\n";
+    // } else {
+    //     cout << "Non Prime Number\n";
+    // }
+
+    // Nested Loops (Introduced)
+    // Loop inside Loop
+    // for(int i=1; i<=5; i++){
+    //     cout << "*****" << endl;
+    // }
+
+    // int x = 10;
+    // for(int i=0; i<=x; i++){
+    //     cout << "*";
+    // }
+    // cout << endl;
+
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     int x = 10;
+    //     for (int i = 0; i <= x; i++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }   
+
+    // int n = 20;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     int x = 10;
+    //     for (int j = 0; j <= x; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // } 
+
+    // return 0;
+
+    // Homework Problems
+    // (Sum of numbers 1 to n) Using While Loop
+    // (Sum of all Odd Numbers from 1 to N) Using While Loop
+    // (Sum of all Even Numbers from 1 to N) Using For And While Loop
+    // (Sum of all numbers from 1 to N which are divisible by 3) // For And While
+    // (Print factorial of a number N)
+}
